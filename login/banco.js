3568a4c3372d299e4191851324400eb67e90408b
Ermersongomes <ermersonwwe@gmail.com>
2025-02-23 16:33:48 -0300

    new update

---
+++ login/banco.js
@@ -0,0 +1,42 @@
+const bancoDeDados = {
+    "12345": {
+        senha: "senha123",
+        home: "home1.html"
+    },
+    "23456": {
+        senha: "senha456",
+        home: "home2.html"
+    },
+    "34567": {
+        senha: "senha789",
+        home: "home3.html"
+    },
+    "45678": {
+        senha: "senha1011",
+        home: "home4.html"
+    },
+    "56789": {
+        senha: "senha1213",
+        home: "home5.html"
+    },
+    "67890": {
+        senha: "senha1415",
+        home: "home6.html"
+    },
+    "78901": {
+        senha: "senha1617",
+        home: "home7.html"
+    },
+    "89012": {
+        senha: "senha1819",
+        home: "home8.html"
+    },
+    "90123": {
+        senha: "senha2021",
+        home: "home9.html"
+    },
+    "01234": {
+        senha: "senha2223",
+        home: "home10.html"
+    }
+};
