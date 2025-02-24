27f1636af50e66f8de2579dd5f7e2d1720baf6ec
Ermersongomes <ermersonwwe@gmail.com>
2025-02-23 21:36:23 -0300

    Cadastro de novos usuários na tabela

--- login/banco.js
+++ login/banco.js
@@ -1,42 +1,42 @@
 const bancoDeDados = {
     "12345": {
         senha: "senha123",
-        home: "home1.html"
+        home: "/home/home1.html"
     },
     "23456": {
         senha: "senha456",
-        home: "home2.html"
+        home: "/home/home2.html"
     },
     "34567": {
         senha: "senha789",
-        home: "home3.html"
+        home: "/home/home3.html"
     },
     "45678": {
         senha: "senha1011",
-        home: "home4.html"
+        home: "/home/home4.html"
     },
     "56789": {
         senha: "senha1213",
-        home: "home5.html"
+        home: "/home/home5.html"
     },
     "67890": {
         senha: "senha1415",
-        home: "home6.html"
+        home: "/home/home6.html"
     },
     "78901": {
         senha: "senha1617",
-        home: "home7.html"
+        home: "/home/home7.html"
     },
     "89012": {
         senha: "senha1819",
-        home: "home8.html"
+        home: "/home/home8.html"
     },
     "90123": {
         senha: "senha2021",
-        home: "home9.html"
+        home: "/home/home9.html"
     },
     "01234": {
         senha: "senha2223",
-        home: "home10.html"
+        home: "/home/home10.html"
     }
 };
